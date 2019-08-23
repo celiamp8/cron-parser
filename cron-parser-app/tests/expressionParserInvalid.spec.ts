bf7411125940f2767302f59d0feb414e340c651b
Celia Martin <cmartinperez@mimecast.com>
2019-08-23 15:40:54 +0100

    add given when then to tests

--- cron-parser-app/tests/expressionParserInvalid.spec.ts
+++ cron-parser-app/tests/expressionParserInvalid.spec.ts
@@ -3,9 +3,11 @@ import ExpressionParser from '../src/expressionParser';
 
 describe('invalid values', function () {
   it('single numbers out of range', function () {
-    // When
-    let result = new ExpressionParser(" 66 24 32 13 0 command/invalid/single ").getFormattedExpression();
-    // Then
+    // Given input number out of the allowed values range
+    const expString = " 66 24 32 13 0 command/invalid/single ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output 'ERROR: Input out of range'
     expect(result).equal(
       `Minutes: ERROR: Input out of range
 Hours: ERROR: Input out of range
@@ -17,9 +19,11 @@ Command: command/invalid/single`
   });
 
   it('comma separated numbers out of range', function () {
-    // When
-    let result = new ExpressionParser(" 4,33,77 90,100,300,400 3,35 0,7 8,1 command/invalid/comma ").getFormattedExpression();
-    // Then
+    // Given input one or many invalid values in comma separated list
+    const expString = " 4,33,77 90,100,300,400 3,35 0,7 8,1 command/invalid/comma ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output 'ERROR: Some of the provided values are invalid'
     expect(result).equal(
       `Minutes: ERROR: Some of the provided values are invalid
 Hours: ERROR: Some of the provided values are invalid
@@ -31,9 +35,11 @@ Command: command/invalid/comma`
   });
 
   it('interval first values out of range', function () {
-    // When
-    let result = new ExpressionParser(" 60-3 55-* 30-10 6-3 0-1 command/invalid/firstinterval ").getFormattedExpression();
-    // Then
+    // Given input value out of allowed range as first in an interval
+    const expString = " 60-3 55-* 30-10 6-3 0-1 command/invalid/firstinterval ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output 'ERROR: First input of interval out of range'
     expect(result).equal(
       `Minutes: ERROR: First input of interval out of range
 Hours: ERROR: First input of interval out of range
@@ -45,9 +51,11 @@ Command: command/invalid/firstinterval`
   });
 
   it('interval last values out of range', function () {
-    // When
-    let result = new ExpressionParser(" 20-95 *-79 20-89 1-15 *-70 command/invalid/lastinterval ").getFormattedExpression();
-    // Then
+    // Given input value out of allowed range as last in an interval
+    const expString = " 20-95 *-79 20-89 1-15 *-70 command/invalid/lastinterval ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output 'ERROR: Last input of interval out of range'
     expect(result).equal(
       `Minutes: ERROR: Last input of interval out of range
 Hours: ERROR: Last input of interval out of range
@@ -59,9 +67,11 @@ Command: command/invalid/lastinterval`
   });
 
   it('non expected characters', function () {
-    // When
-    let result = new ExpressionParser(" [] (9-14) */sth 1-5= etc command/invalid/nonexpected ").getFormattedExpression();
-    // Then
+    // Given input not allowed characters (chars other than 0-9 , * / and ?)
+    const expString = " [] (9-14) */sth 1-5= etc command/invalid/nonexpected ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output 'ERROR: Invalid expression'
     expect(result).equal(
       `Minutes: ERROR: Invalid expression
 Hours: ERROR: Invalid expression
--- cron-parser-app/tests/expressionParserValid.spec.ts
+++ cron-parser-app/tests/expressionParserValid.spec.ts
@@ -3,9 +3,11 @@ import ExpressionParser from '../src/expressionParser';
 
 describe('valid values', function () {
   it('single values', function () {
-    // When
-    let result = new ExpressionParser(" 30 13 8 1 3 command/single ").getFormattedExpression();
-    // Then
+    // Given input of single value
+    const expString = " 30 13 8 1 3 command/single ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output single value
     expect(result).equal(
       `Minutes: 30
 Hours: 13
@@ -17,9 +19,11 @@ Command: command/single`
   });
 
   it('all', function () {
-    // When
-    let result = new ExpressionParser(" * * * * * command/all ").getFormattedExpression();
-    // Then
+    // Given input of *
+    const expString = " * * * * * command/all ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output all possible values
     expect(result).equal(
       `Minutes: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59
 Hours: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
@@ -31,9 +35,11 @@ Command: command/all`
   });
 
   it('any', function () {
-    // When
-    let result = new ExpressionParser(" ? ? ? ? ? command/any ").getFormattedExpression();
-    // Then
+    // Given input of ?
+    const expString = " ? ? ? ? ? command/any ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output 'Any'
     expect(result).equal(
       `Minutes: Any
 Hours: Any
@@ -45,9 +51,11 @@ Command: command/any`
   });
 
   it('intervals', function () {
-    // When
-    let result = new ExpressionParser(" *-12 4-16 3-30 2-7 4-5 command/intervals ").getFormattedExpression();
-    // Then
+    // Given input of intervals
+    const expString = " *-12 4-16 3-30 2-7 4-5 command/intervals ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output of all nums in interval including first and last
     expect(result).equal(
       `Minutes: 0 1 2 3 4 5 6 7 8 9 10 11 12
 Hours: 4 5 6 7 8 9 10 11 12 13 14 15 16
@@ -59,9 +67,11 @@ Command: command/intervals`
   });
 
   it('increments', function () {
-    // When
-    let result = new ExpressionParser(" */20 4-22/10 *-29/3 */6 3-*/2 command/increments ").getFormattedExpression();
-    // Then
+    // Given input with increment
+    const expString = " */20 4-22/10 *-29/3 */6 3-*/2 command/increments ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output expected values looped with given increment
     expect(result).equal(
       `Minutes: 0 20 40
 Hours: 4 14
@@ -73,9 +83,11 @@ Command: command/increments`
   });
 
   it('comma separated values', function () {
-    // When
-    let result = new ExpressionParser(" 1,15,30 4,5,6 7,30 3,4,5,8,9,11 5,6 command/comma ").getFormattedExpression();
-    // Then
+    // Given comma separated values
+    const expString = " 1,15,30 4,5,6 7,30 3,4,5,8,9,11 5,6 command/comma ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output values in the given order separated by space
     expect(result).equal(
       `Minutes: 1 15 30
 Hours: 4 5 6
@@ -87,9 +99,11 @@ Command: command/comma`
   });
 
   it('multiple values', function () {
-    // When
-    let result = new ExpressionParser(" 1-40 */5 3-*/2 *-4 ? command/multivals ").getFormattedExpression();
-    // Then
+    // Given a combination of intervals and increments
+    const expString = " 1-40 */5 3-*/2 *-4 ? command/multivals ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output values in interval looped with given increment
     expect(result).equal(
       `Minutes: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40
 Hours: 0 5 10 15 20
@@ -101,9 +115,11 @@ Command: command/multivals`
   });
 
   it('all with manual intervals', function () {
-    // When
-    let result = new ExpressionParser(" 0-59 0-23 *-31 1-* 1-7 command/allmanual ").getFormattedExpression();
-    // Then
+    // Given input all possible values with intervals
+    const expString = " 0-59 0-23 *-31 1-* 1-7 command/allmanual ";
+    // When parse expression
+    let result = new ExpressionParser(expString).getFormattedExpression();
+    // Then output all possible values 
     expect(result).equal(
       `Minutes: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59
 Hours: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
